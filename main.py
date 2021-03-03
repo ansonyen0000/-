130e3d3130fb9a6638b02a450910cd4a1daf417d
ansonyen0000 <79684333+ansonyen0000@users.noreply.github.com>
2021-03-03 22:50:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myMath import myStatistics
+B = int(input('data 1 :'))
+C = int(input('data 2 :'))
+D = int(input('data 3 :'))
+E = int(input('data 4 :'))
+F = int(input('data 5 :'))
+A = [B,C,D,E,F]
+print('平均為:',myStatistics.myMean(A))
